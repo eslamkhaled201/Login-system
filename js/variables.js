dc68cab0b549827aaa8712babcefd7b777759884
eslamkhaled201 <59029234+eslamkhaled201@users.noreply.github.com>
2020-12-26 21:16:43 +0200

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,7 @@ let passwordInput =  $("#userPassIn");
 let userNameInput =  $("#userNameIn");
 let signInBtn = $("#signInBtn");
 let signUpBtn =  $("#signUpBtn");
-let URLOrigin = `${location.origin}${location.pathname}`; 
+let URLOrigin = `${location.origin}/Login-system/`; 
 
 export{
     emailInput, 
